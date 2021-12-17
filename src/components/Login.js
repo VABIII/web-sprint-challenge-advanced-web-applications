2fce38f137fd1a60b0d7df693665c5adcc02ecc8
VABIII <tbengtson82@gmail.com>
2021-12-16 21:12:09 -0600

    login form built out and setting token in localStorage

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 import styled from 'styled-components';
-
-const Login = () => {
-    
-    return(<ComponentContainer>
-        <ModalContainer>
-            <h1>Welcome to Blogger Pro</h1>
-            <h2>Please enter your account information.</h2>
-        </ModalContainer>
-    </ComponentContainer>);
-}
-
-export default Login;
-
-//Task List
-//1. Build login form DOM from scratch, making use of styled components if needed. Make sure the username input has id="username" and the password input as id="password".
-//2. Add in a p tag with the id="error" under the login form for use in error display.
-//3. Add in necessary local state to support login form and error display.
-//4. When login form is submitted, make an http call to the login route. Save the auth token on a successful response and redirect to view page.
-//5. If the response is not successful, display an error statement. **a server provided error message can be found in ```err.response.data```**
-//6. MAKE SURE TO ADD id="username", id="password", id="error" AND id="submit" TO THE APPROPRIATE DOM ELEMENTS. YOUR AUTOTESTS WILL FAIL WITHOUT THEM.
 
 const ComponentContainer = styled.div`
     height: 70%;
@@ -55,3 +36,86 @@ const Button = styled.button`
     padding:1rem;
     width: 100%;
 `
+const initialValues = {
+    username: "Lambda",
+    password: "School"
+}
+
+
+const Login = () => {
+    const [values, setValues] = useState(initialValues);
+    const [error, setError] = useState("");
+    // console.log(values);
+
+    const login = () => {
+        axios.post("http://localhost:5000/api/login", values)
+            .then(res => {
+                console.log(res)
+                const token = res.data.token;
+                const role = res.data.role;
+                const username = res.data.username;
+
+                localStorage.setItem("token", token);
+                localStorage.setItem("role", role);
+                localStorage.setItem("username", username);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    } ;
+
+    const onChange = evt => {
+        const name = evt.target.name;
+        const value = evt.target.value;
+        setValues({
+            ...values,
+            [name]: value
+        });
+    };
+
+    const onSubmit = evt => {
+        evt.preventDefault();
+        login();
+    };
+
+    return(
+        <ComponentContainer>
+            <ModalContainer>
+                <h1>Welcome to Blogger Pro</h1>
+                <h2>Please enter your account information.</h2>
+            </ModalContainer>
+            <div>
+                <FormGroup onSubmit={onSubmit}>
+                    <Label>Username:&nbsp;</Label>
+                    <Input
+                        type="text"
+                        name="username"
+                        value={values.username}
+                        onChange={onChange}
+                    />
+                    <Label>Password:&nbsp;</Label>
+                    <Input
+                        type="text"
+                        name="password"
+                        value={values.password}
+                        onChange={onChange}
+                    />
+                    <Button>Login</Button>
+                </FormGroup>
+                <p></p>
+            </div>
+        </ComponentContainer>
+    );
+}
+
+export default Login;
+
+//Task List
+//1. Build login form DOM from scratch, making use of styled components if needed. Make sure the username input has id="username" and the password input as id="password".
+//2. Add in a p tag with the id="error" under the login form for use in error display.
+//3. Add in necessary local state to support login form and error display.
+//4. When login form is submitted, make an http call to the login route. Save the auth token on a successful response and redirect to view page.
+//5. If the response is not successful, display an error statement. **a server provided error message can be found in ```err.response.data```**
+//6. MAKE SURE TO ADD id="username", id="password", id="error" AND id="submit" TO THE APPROPRIATE DOM ELEMENTS. YOUR AUTOTESTS WILL FAIL WITHOUT THEM.
+
+
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,10 +2,52 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 function PrivateRoute() {
+
+
+
+
   return (<Route/>);
 }
 
 export default PrivateRoute;
 
-//Task List:
-//1. Complete PrivateRoute
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
